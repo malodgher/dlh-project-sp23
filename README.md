b2c3ce817a37d9d17627f9a6535a6eba9313c5d8
sanir53 <sagardd2@illinois.edu>
2023-05-06 10:18:42 -0400

    Initial Commit by Sagar with working code

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
 # dlh-project-sp23
 CS598 Deep Learning for Healthcare Spring 2023 project code by Murtaza Lodgher and Sagar Dalwadi
+
+There are 2 sections: 1) Used Author's code 2) Implemented our own code
+
+# ###### Author's Code #######
+## (Under Authors code folder)
+
+The first step of our implementation involved using the original code base provided in the paper and making the necessary updates to the code to make it compatible with the latest version of Python including necessary updates to the libraries used such that we are able to successfully run the model against the data and produce the results.
+
+We also utilized the synthetic data provided and passed it through the model for testing purposes and successfully generated the AUC for each epoch.
+
+## Heart Failure Prediction using RNN
+This is a simple RNN (implemented with Gated Recurrent Units) for predicting a HF diagnosis given patient records.
+
+There are four different versions:
+
+1. gru_onehot.py: This uses one-hot encoding for the medical code embedding
+2. gru_onehot_time.py: This uses one-hot encoding for the medical code embedding. This uses time information in addition to the code sequences
+3. gru_emb.py: This uses pre-trained medical code embeddings. 
+4. gru_emb_time.py: This uses pre-trained medical code embeddings. This suses time information in addition to the code sequences.
+
+The data are synthetic and make no sense at all. It is intended only for testing the codes.
+
+1. sequences.pkl: This is a pickled list of list of integers. Each integer is assumed to be some medical code.
+2. times.pkl: This is a pickled list of list of integers. Each integer is assumed to the time at which the medical code occurred.
+3. labels.pkl: This is a pickled list of 0 and 1s.
+4. emb.pkl: This is a randomly generated code embedding of size 100 X 100
+
+## Requirement
+Python and Theano are required to run the scripts
+
+## How to Execute
+1. python gru_onehot.py sequences.pkl labels.pkl <output>
+2. python gru_onehot_time.py sequences.pkl times.pkl labels.pkl <output>
+3. python gru_emb.py sequences.pkl labels.pkl emb.pkl <output>
+4. python gru_emb_time.py sequences.pkl times.pkl labels.pkl emb.pkl <output>
+
+## Reference
+https://github.com/mp2893/rnn_predict
+https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5391725/pdf/ocw112.pdf
+
+
+# ########### Student's Code (Implemented our own) ##########
+## (Under Student code folder)
+## Implemented by Sagar Dalwadi - sagardd2
+## Corresponding Jupyter Notebook is provided: CS598_DLH_Project_Sagar.ipynb
+
+Now that we experimented with Author's code and got an understanding of how the model works & what data should be used, I implemented my own code with python keras library and real world data from PhysioNet website.
+
+# Heart failure detection model usig RNN
+Similar to the model provided in paper, this is a simple RNN with GRU for predicting heart failure for patient records.
+
+There are 2 different versions:
+
+1. rnn_gru_sigmoid.py: This uses sigmoid as activation function as part of RNN with GRU model. This uses admissions, patients, diagnoses, icu stays data from PhysioNet
+2. rnn_gru_relu.py: This implements one of the ablations we proposed and uses ReLu activation function as part of RNN with GRU model. In addition to dataset mentioned in 1st version, it also uses lab events data for patients.
+
+# Data
+
+Data sets are extracted from PhysioNet website: (you need to have "CITI Data or Specimens Only Research" training completed & approved before you can access the data)
+https://physionet.org/content/mimiciii/1.4/
+
+Once you have access go to Files section on above link where you can download required data files.
+
+1. Admissions: gives information regarding a patient’s admission to the hospital
+2. Patients: Provides information on each patient
+3. Diagnoses ICD: Contains ICD diagnoses for patients
+4. ICU Stays: defines a single ICU stay
+5. Lab events: Contains all laboratory measurements for a given patient
+
+More information on dataset, fields etc can be found at: https://mimic.mit.edu/docs/iii/tables/
+
+# Requirements / Pre-requisite
+
+Install Python & required libraries (Code file includes a subsection for installation using PIP)
+
+# How to Execute
+
+Before you execute the code, you need to set couple of parameters in the code file as follows:
+
+1. data_dir: Once you download the data files as instructed above, place them in desired folder on your machine and provide a directory path here.
+2. labevents_records: If you are executiong Version 2 above (rnn_gru_relu.py) then you need to set this according to the computation power you have on your machine. Default is 500k (500000) and Max is 27.8M (27854060). If you are executing on personal computer then I would recommend keep it as default.
+3. inputdf_records: Set this according to the computation power of your machine. Default is 15k and Max is 1.4M for Verison 1 and 200k for Version 2. If you are executing on personal computer then I would recommend keep it as default.
+4. max_epoch: Default is 3. You can set this as per your preference.
+
+Executing:
+
+1. Python rnn_gru_simoid.py
+2. Python rnn_gru_relu.py
+
---
+++ Student code/rnn_gru_sigmoid.py
@@ -0,0 +1,261 @@
+
+#---------------------*********************************--------------------------------#
+#---------------------*********************************--------------------------------#
+
+# ***** Implemented by: Sagar dalwadi
+# ***** Net ID: sagardd2
+
+#---------------------*********************************--------------------------------#
+#---------------------*********************************--------------------------------#
+
+#!/usr/bin/env python
+# coding: utf-8
+
+#---------------------*********************************--------------------------------#
+############# Install required libraries #############################
+#---------------------*********************************--------------------------------#
+
+
+#pip install pyhealth
+
+
+#pip install pyreadr
+
+#pip install pandas
+
+#pip install sklearn
+
+#pip install tensorflow
+
+
+#---------------------*********************************--------------------------------#
+################### Importing required modules/libraries ############################
+#---------------------*********************************--------------------------------#
+
+from pyhealth.datasets import MIMIC3Dataset
+import pyreadr
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from tensorflow.keras.layers import GRU, Dense, Dropout, Embedding
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import classification_report
+
+
+#---------------------*********************************--------------------------------#
+######################## Testing with pyhealth dataset ###################
+#---------------------*********************************--------------------------------#
+
+# create MIMIC3Dataset object
+mimic3base = MIMIC3Dataset(
+    root="https://storage.googleapis.com/pyhealth/Synthetic_MIMIC-III/",
+    tables=["DIAGNOSES_ICD", "PROCEDURES_ICD", "PRESCRIPTIONS"],
+    # map all NDC codes to ATC 3-rd level codes in these tables
+    code_mapping={"NDC": ("ATC", {"target_kwargs": {"level": 3}})},
+)
+
+
+mimic3base.stat()
+
+########################################################
+
+
+#---------------------*********************************--------------------------------#
+################### Input data preprocessing ############################
+#---------------------*********************************--------------------------------#
+
+
+
+data_dir = "/Users/sagardalwadi/Desktop/College/CS598-DLH/project/DLH_Project_CodeRepo_By_sagardd2/Data/"
+inputdf_records = 15000
+max_epoch = 3
+
+# load the DIAGNOSES_ICD table as a Pandas dataframe
+diagnoses_icd_df = pd.read_csv(data_dir + "DIAGNOSES_ICD.csv")
+
+diagnoses_icd_df.head()
+
+# get the statistics of the dataframe
+diagnoses_icd_stats = diagnoses_icd_df.describe()
+
+# print the statistics
+print(diagnoses_icd_stats)
+
+# load the PROCEDURES_ICD table as a Pandas dataframe
+procedures_icd_df = pd.read_csv(data_dir + "PROCEDURES_ICD.csv")
+
+# load the PRESCRIPTIONS table as a Pandas dataframe
+prescriptions_df = pd.read_csv(data_dir + "PRESCRIPTIONS.csv")
+
+
+# get the statistics of the dataframe
+procedures_icd_stats = procedures_icd_df.describe()
+
+# print the statistics
+print(procedures_icd_stats)
+
+# get the statistics of the dataframe
+prescriptions_stats = prescriptions_df.describe()
+
+# print the statistics
+print(prescriptions_stats)
+
+# load the ADMISSIONS table as a Pandas dataframe
+admissions_df = pd.read_csv(data_dir + "ADMISSIONS.csv")
+
+
+# get the statistics of the dataframe
+admissions_stats = admissions_df.describe()
+
+# print the statistics
+print(admissions_stats)
+admissions_df.head()
+
+
+# load the ICUSTAYS table as a Pandas dataframe
+icustays_df = pd.read_csv(data_dir + "ICUSTAYS.csv")
+
+# get the statistics of the dataframe
+icustays_stats = icustays_df.describe()
+
+# print the statistics
+print(icustays_stats)
+
+# load the PATIENTS table as a Pandas dataframe
+patients_df = pd.read_csv(data_dir + "PATIENTS.csv")
+
+# get the statistics of the dataframe
+patients_stats = patients_df.describe()
+
+# print the statistics
+print(patients_stats)
+
+# merge the tables together based on the common patient identifier
+patient_admissions = pd.merge(admissions_df, patients_df, on="SUBJECT_ID")
+patient_admissions_icd = pd.merge(patient_admissions, diagnoses_icd_df, on=["SUBJECT_ID", "HADM_ID"])
+patient_admissions_icustays = pd.merge(patient_admissions_icd, icustays_df, on=["SUBJECT_ID", "HADM_ID"])
+
+# print the resulting dataframe
+print(patient_admissions_icustays.head())
+
+# Define heart failure ICD codes
+hf_codes = ['4280', '4281', '4289','42820','42821','42822','42823','42830','42831','42832','42833']
+
+# Create new column for heart failure flag
+patient_admissions_icustays['heart_failure'] = np.where(patient_admissions_icustays['ICD9_CODE'].isin(hf_codes) , 1, 0)
+
+patient_admissions_icustays.head()
+
+
+columns = patient_admissions_icustays.columns.tolist()
+print(columns)
+
+input_df = patient_admissions_icustays[['SUBJECT_ID','MARITAL_STATUS','DIAGNOSIS','GENDER','ICD9_CODE','LOS','heart_failure']]
+
+input_df.head()
+input_onehot_df = input_df
+
+input_onehot_df.head()
+input_onehot_df = input_onehot_df.drop_duplicates()
+
+
+#---------------------*********************************--------------------------------#
+################### One hot encoding ############################
+#---------------------*********************************--------------------------------#
+
+
+
+
+one_hot = pd.get_dummies(input_onehot_df['MARITAL_STATUS'])
+input_onehot_df = input_onehot_df.drop('MARITAL_STATUS', axis=1)
+input_onehot_df = input_onehot_df.join(one_hot)
+
+one_hot = pd.get_dummies(input_onehot_df['DIAGNOSIS'])
+input_onehot_df = input_onehot_df.drop('DIAGNOSIS', axis=1)
+input_onehot_df = input_onehot_df.join(one_hot)
+
+one_hot = pd.get_dummies(input_onehot_df['GENDER'])
+input_onehot_df = input_onehot_df.drop('GENDER', axis=1)
+input_onehot_df = input_onehot_df.join(one_hot)
+
+
+input_onehot_df = input_onehot_df.drop('ICD9_CODE', axis=1)
+#input_onehot_df = input_onehot_df.join(one_hot)
+
+input_onehot_df = input_onehot_df.drop('LOS', axis=1)
+input_onehot_df = input_onehot_df.drop('SUBJECT_ID', axis=1)
+
+input_onehot_df.head()
+input_limited_df = input_onehot_df.loc[:inputdf_records, :]
+
+
+
+#---------------------*********************************--------------------------------#
+################### RNN model implementation ############################
+#---------------------*********************************--------------------------------#
+
+
+
+
+# Split the data into input (X) and target (y) variables
+X = input_limited_df.drop(['heart_failure'], axis=1)
+y = input_limited_df['heart_failure']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+
+X_train.astype('float32')
+X_test.astype('float32')
+
+# Define the number of features and the length of the input sequences
+num_features = X_train.shape[1]
+seq_length = 1
+
+# Define the model architecture
+model = Sequential()
+model.add(Embedding(input_dim=2, output_dim=num_features, input_length=seq_length))
+model.add(GRU(units=32, input_shape=(seq_length, num_features)))
+model.add(Dense(units=1, activation='sigmoid'))
+model.summary()
+
+# Define the model architecture
+#model = Sequential()
+#model.add(Embedding(input_dim=2, output_dim=num_features, input_length=seq_length))
+#model.add(GRU(units=32, activation='relu', input_shape=(seq_length, num_features)))
+#model.add(Dense(units=16, activation='relu'))
+#model.add(Dropout(0.2))
+#model.add(Dense(units=1, activation='sigmoid'))
+
+# Compile the model
+optimizer = Adam(lr=0.001)
+model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+#model.summary()
+
+# Fit the model to the training data
+#history = model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
+
+#model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#model.fit(X_train.values.reshape(-1, 1, num_features), y_train.values, epochs=3, batch_size=5, validation_data=(X_test.values.reshape(-1, 1, num_features), y_test.values))
+
+model.fit(X_train, y_train, epochs=max_epoch, batch_size=5, validation_data=(X_test, y_test))
+
+
+# Evaluate the model on the testing set
+loss, accuracy = model.evaluate(X_test, y_test)
+print('Test accuracy:', accuracy)
+
+# Get the model predictions on the testing set
+y_pred = model.predict(X_test)
+
+# Convert predicted probabilities to class labels
+y_pred_labels = np.round(y_pred)
+
+# Print classification report
+print(classification_report(y_test, y_pred_labels, zero_division=1))
+
+
+
